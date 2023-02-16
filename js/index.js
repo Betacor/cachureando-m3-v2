cb24977b3daa57a53dd248d49186d77ccff022c7
Sappy <macarenaramdohr@gmail.com>
2023-02-16 12:01:52 -0300

    index arreglos

--- js/index.js
+++ js/index.js
@@ -11,3 +11,29 @@ $(window).resize(function () {
         $('nav ul').removeAttr('style');
     }
 });
+
+function clock() {
+    const today = new Date();
+    let h = toTwoDigits(today.getHours());
+    let m = toTwoDigits(today.getMinutes());
+    let s = toTwoDigits(today.getSeconds());
+    let month = toTwoDigits(today.getMonth() + 1);
+    let year = today.getFullYear();
+    let day = toTwoDigits(today.getDate());
+
+    document.getElementById("clock").innerHTML = h + ":" + m + ":" + s + "  " + day + "-" + month + "-" + year;
+
+    setTimeout(clock, 1000);
+}
+function toTwoDigits(n) {
+    let newString = n;
+    if (n < 10) {
+        newString = "0" + newString;
+    }
+    return newString;
+}
+function mostrar() {
+    swal("Seguro que deseas agregar al carrito",{
+        buttons: ["Si", "No"]
+    });
+}
